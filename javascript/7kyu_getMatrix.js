d3cbc5ab2cb8a6b0528ff23b6857f9269aae3afa
FRIDA LEMKE <frida.lemke@gmx.de>
2024-12-02 13:10:16 +0100

    add daily kata

---
+++ javascript/7kyu_getMatrix.js
@@ -0,0 +1,11 @@
+function getMatrix(number) {
+  const matrix = [];
+  for (let i = 0; i < number; i++) {
+    const row = [];
+    for (let j = 0; j < number; j++) {
+      row.push(i === j ? 1 : 0);
+    }
+    matrix.push(row);
+  }
+  return matrix;
+}
