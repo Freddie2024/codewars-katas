56e3e1ddaf545dfce50f9126b80574e8999aef57
FRIDA LEMKE <frida.lemke@gmx.de>
2025-01-11 22:29:48 +0100

    add daily kata

--- typescript/8kyu_stairsIn20.ts
+++ typescript/8kyu_stairsIn20.ts
@@ -1,3 +1,3 @@
-function stairsIn20(stairs:number[][]) : number {
+export function stairsIn20(stairs:number[][]) : number {
     return stairs.flat().reduce((sum, dailyStairs) => sum + dailyStairs, 0) * 20
   }
