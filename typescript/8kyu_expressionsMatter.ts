962ee42ecdc2851e385d34498d77ae0a3a1d27a8
FRIDA LEMKE <frida.lemke@gmx.de>
2025-02-10 21:26:40 +0100

    add daily kata

---
+++ typescript/8kyu_expressionsMatter.ts
@@ -0,0 +1,10 @@
+export function expressionsMatter(a: number, b: number, c: number): number {
+    return Math.max(
+      a + b + c,
+      a * b * c,
+      (a + b) * c,
+      a * (b + c),
+      a + (b * c),
+      (a * b) + c
+    );
+  }
