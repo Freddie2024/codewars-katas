c69940c3d78b2dfb52b98f2d519cd1132177d5e4
FRIDA LEMKE <frida.lemke@gmx.de>
2025-03-11 22:25:41 +0100

    add daily kata

---
+++ typescript/8kyu_firstNonConsecutive.ts
@@ -0,0 +1,8 @@
+export function firstNonConsecutive (arr: number[]) : null | number {
+    for(let i=1; i<arr.length; i++) {
+      if (arr[i] !== arr[i-1]+1) {
+        return arr[i];
+      }
+    }
+    return null;
+  }
